061b70db79337d65d94e8e7b53a4c5eb9e4d08a7
Darwin <balldarwin638@gmail.com>
2025-05-29 21:48:54 -0600

    cambios para frontend render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,13 @@
 import axios from 'axios'
 
+// const api = axios.create({
+//   baseURL: import.meta.env.DEV
+//     ? '/api'                // usa el proxy en desarrollo
+//     : 'http://localhost:5000'
+// })
 const api = axios.create({
-  baseURL: import.meta.env.DEV
-    ? '/api'                // usa el proxy en desarrollo
-    : 'http://localhost:5000'
-})
-
+  baseURL: import.meta.env.VITE_BACKEND_URL
+});
 // Prueba de conexión
 export const ping = () => api.get('/ping')
 
